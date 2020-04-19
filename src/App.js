d6bd5b3d10c0c038c75500f6626ce42138fcda7f
Miguel Cobas <mcobas@dealergeek.com>
2020-04-19 00:23:45 -0500

    Se agrego el api para buscar la letra y el state para almacenarlo, se instalo axios para hacer la peticion mas sencilla

--- src/App.js
+++ src/App.js
@@ -1,11 +1,31 @@
-import React, {Fragment} from 'react';
+import React, {Fragment, useState, useEffect} from 'react';
 import Formulario from './componentes/Formulario'
-
+import axios from 'axios'
 function App() {
+
+  const [ buscarletra, guardarBuscarLetra]= useState({});
+  const [letra, guardarLetra] = useState('');
+
+  useEffect(()=>{
+    if(Object.keys(buscarletra).length === 0) return
+
+    const consultarApiLetra = async () =>{
+
+      const {artista,cancion}=buscarletra;
+
+      const url = `https://api.lyrics.ovh/v1/${artista}/${cancion}`
+
+      const resultado = await axios.get(url)
+      guardarLetra(resultado.data.lyrics)
+    }
+    consultarApiLetra()
+  },[buscarletra])
   return (
     
     <Fragment>
-      <Formulario />
+      <Formulario
+        guardarBuscarLetra={guardarBuscarLetra}
+      />
     </Fragment>
   );
 }

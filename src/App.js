b534f96f4bd377a8015019451d9f6d9020515f1a
Miguel Cobas <mcobas@dealergeek.com>
2020-04-19 01:21:30 -0500

    Se termino el detalle de la info del artista

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React, {Fragment, useState, useEffect} from 'react';
 import Formulario from './componentes/Formulario'
-import axios from 'axios'
 import Cancion from './componentes/Cancion';
+import Info from './componentes/Info';
+import axios from 'axios'
+
 function App() {
 
   const [ buscarletra, guardarBuscarLetra]= useState({});
   const [letra, guardarLetra] = useState('');
+  const  [info, guardarInfo] = useState([]);
 
   useEffect(()=>{
     if(Object.keys(buscarletra).length === 0) return
@@ -15,12 +18,18 @@ function App() {
       const {artista,cancion}=buscarletra;
 
       const url = `https://api.lyrics.ovh/v1/${artista}/${cancion}`
+      const url2 = `https://www.theaudiodb.com/api/v1/json/1/search.php?s=${artista}`
 
-      const resultado = await axios.get(url)
-      guardarLetra(resultado.data.lyrics)
+      // HACER LA PETICION A 2 APIS SIMULTANEOS SIN QUE DEPENDA UNA DE LA OTRA SE USA PROMISE Y ES POR MEDIO DE ARREGLOS
+      const [ letra, informacion ] = await Promise.all([
+        axios.get(url), axios.get(url2)
+      ]  )
+
+      guardarLetra(letra.data.lyrics)
+      guardarInfo(informacion.data.artists[0])
     }
     consultarApiLetra()
-  },[buscarletra])
+  },[buscarletra, info])
   return (
     
     <Fragment>
@@ -31,6 +40,9 @@ function App() {
         <div className="row">
           <div className="col-md-6"
           >
+          <Info 
+           info={info}
+          />
 
           </div>
           <div className="col-md-6"
